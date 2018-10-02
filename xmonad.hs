8dc6bf96698b96a28de442683706e8db69ad94e3
Marc Päpper <m.paepper@lemundo.de>
2018-10-02 08:52:57 +0200

    Removed vpn desktop view as this is now a command line script

--- xmonad.hs
+++ xmonad.hs
@@ -49,7 +49,7 @@ myLauncher = "$(~/.cabal/bin/yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-
 -- Workspaces
 -- The default number of workspaces (virtual screens) and their names.
 --
-myWorkspaces = ["1:term","2:comm.","3:pw","4:db","5:vpn"] ++ map show [6..9]
+myWorkspaces = ["1:term","2:comm.","3:pw","4:db"] ++ map show [6..9]
 
 
 ------------------------------------------------------------------------
@@ -81,7 +81,6 @@ myManageHook = composeAll
     , className =? "Keepassx"           --> doShift "3:pw"
     , className =? "Mattermost"              --> doShift "2:comm."
     , className =? "Mysql-workbench-bin" --> doShift "4:db"
-    , className =? "Qikea"              --> doShift "5:vpn"
     , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
 
 
